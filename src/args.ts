e32151afd362cf35dc44fdc2b71da591774bbd99
Josh Newman <git@technicallyjosh.com>
2019-12-18 13:05:37 -0700

    add groups for args help

--- src/args.ts
+++ src/args.ts
@@ -15,45 +15,53 @@ export default yargs
     type: 'string',
     choices: ['json', 'ndjson'],
     desc: 'type of body to send',
+    group: 'Sending',
     default: 'json',
   })
   .option('url', {
     type: 'string',
     require: true,
+    group: 'Sending',
     desc: 'url to send logs to',
   })
   .option('username', {
     alias: 'u',
     type: 'string',
     desc: 'basic auth username',
+    group: 'Basic Auth',
   })
   .option('password', {
     alias: 'p',
     type: 'string',
     desc: 'basic auth password',
+    group: 'Basic Auth',
   })
   .option('batchSize', {
     alias: 's',
     type: 'number',
     desc: 'how many logs to send at a time',
+    group: 'Sending',
     default: 10,
   })
   .option('retries', {
     alias: 'r',
     type: 'number',
     desc: 'number of retries to do if failure',
+    group: 'Retry',
     default: 5,
   })
   .option('interval', {
     alias: 'i',
     type: 'number',
     desc: 'interval (in ms) to retry sending if failure',
+    group: 'Retry',
     default: 1000,
   })
   .option('timeout', {
     alias: 't',
     type: 'number',
     desc: 'timeout (in ms) to send logs in bucket that are not filled',
+    group: 'Sending',
     default: 5000,
   })
   .option('config', {
